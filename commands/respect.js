e0d22e15bb3028753978954ed1420abd586e84aa
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-07-20 12:56:03 +0700

    Update respect.js

--- commands/respect.js
+++ commands/respect.js
@@ -19,29 +19,29 @@ module.exports = {
     var a = 0;
     targets = [];
     do {
-      targets[a] = imports.built_ins.getMemberFomMention(imports.args.shift(), imports.message)
+      targets[a] = imports.built_ins.getMemberFromMention(imports.args.shift(), imports.message)
       a++;
-    } while (imports.built_ins.getMemberfromMention(imports.args[0]));
+    } while (imports.built_ins.getMemberFromMention(imports.args[0], imports.message));
   
 
-  
+  if(!targets.length)return imports.message.channel.send("Noone to be respected?");
     let gifs = [
       "https://media.discordapp.net/attachments/717210048174096446/719403405625524386/tenor_4.gif",
       "https://media.discordapp.net/attachments/717210048174096446/719398381985726524/tenor_3.gif",
       "https://media.discordapp.net/attachments/717210048174096446/719398360624267324/tenor_1.gif",
       "https://media.discordapp.net/attachments/717210048174096446/719398334120329296/tenor_2.gif"
     ];
-    let selectedGIF = gifs[Math.floor(Math.random() * gifs.length)];
+    gifs = gifs[Math.floor(Math.random() * gifs.length)];
     var the_reason = imports.args.join(" ");
     if(the_reason){
     the_reason = ` because "` + imports.args.join(" ") + `".`;
     } else {
       the_reason = "."
     }
-    let embedkill = imports.Discord.MessageEmbed()
-      .setColor(imports.color.BG_COLOR)
+    let embedkill = new imports.Discord.MessageEmbed()
+      .setColor(imports.colors.BG_COLOR)
       .setAuthor(imports.client.user.username, imports.client.user.displayAvatarURL({format: "png", dynamic: true}))
-      .setDescription(`${message.author} respected ${targets.join(", ")}${the_reason}`)
+      .setDescription(`${imports.message.author} respected ${targets.join(", ")}${the_reason}`)
       .setImage(selectedGIF)
       .setTimestamp()
       .setFooter(`Prefix: ${process.env.PREFIX} | ${imports.built_ins.getRandomFunfact()}`)
