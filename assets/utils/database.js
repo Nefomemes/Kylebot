48b5f2a76cc1419da82a9113ada54e13641ee361
Nefomemes <nefothingy@hotmail.com>
2020-07-28 04:19:33 +0000

    Nice, database work lagi setelah pakai promise.

--- assets/utils/database.js
+++ assets/utils/database.js
@@ -12,24 +12,44 @@ MongoClient.connect(url, function(err, dbClient) {
     module.exports.dbClient = dbClient;
     const userDB = db.collection("users");
 
-function getUser(userID){
-    return db.collection("users").findOne({userID: userID});
-}
-module.exports.getUser = getUser;
-function createUser(userID){
-    if(getUser(userID)) return;
-    return db.collection("users").insertOne({userID: imports.message.author.id});
-}
-module.exports.createUser = createUser;
-function updateUser(userID, status){
-    if(!userID || !status)
-    if(userID.constructor !== String)return;
-    if(!getUser(userID)) createUser(userID);
 
-    db.collection("users").updateOne({userID: userID}, { $set: status,
- $currentDate: { lastModified: true } });
+
+function getUser(userID){
+    return new Promise( async (resolve, reject) => {
+        try {
+        (async function() {
+        var user = await db.collection("users").findOne({userID: userID});
+
+        if(!user) {
+            await db.collection("users").insertOne({userID: userID})
+            resolve( await db.collection("users").findOne({userID: userID}))
+        } else {
+            resolve(user)
+        } 
+        })()
+        }catch(e){
+            reject(e);
+        }
+    })
+}
+
+function updateUser(userID,status){
+    return new Promise( async (resolve, reject) => {
+        try {
+            (async function(){
+            await getUser(userID);
+            await db.collection("users").updateOne({userID: userID}, { $set: status,
+ $currentDate: { lastModified: true } })
+ resolve(await getUser(userID));
+  })()
+        }catch(e){
+            reject(e);
+        }
+    })
 }
 module.exports.updateUser = updateUser;
+module.exports.getUser = getUser;
+
 
   });
 })()
