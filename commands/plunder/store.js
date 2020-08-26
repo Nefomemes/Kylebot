8caa4a9f641f8f97c6ec917146fcf48ae411e513
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-08-26 14:35:50 +0700

    Fix bug: "Store command not working properly"
    Attempt 1

--- commands/plunder/store.js
+++ commands/plunder/store.js
@@ -9,10 +9,9 @@ module.exports = {
             .setAuthor(imports.client.user.username, imports.client.user.displayAvatarURL({ format: "png", dynamic: true }))
             .setTimestamp()
             .setFooter(`Prefix: ${imports.prefix} | You have ${user.cp || 0} CP and  ${user.cash || 0} Plunder cash.`)
-        const bundles = imports.getItem("bundle", null, "all").filter((bundle) => {
-            if (bundle.available && bundle.available === false) return false;
-            return true;
-        })
+        
+        
+       var fields = [];
         if (imports.getItem('bundle', imports.args[0])) {
             const item = imports.getItem('bundle', imports.args[0]);
 
@@ -37,11 +36,12 @@ module.exports = {
                                 default:
                                     rarity = "unknown";
                             }
-                            embed = embed.addField(content.name || "Unknown", `ID: \`${content.id || "<redacted>"}\`\n\nWorth ${content.price || 0} <:cp:744403130594230313>. \n${content.desc || "<redacted>"}\nThis item have the rarity of **${rarity || "unknown"}**.`)
+                            fields.push({name: content.name || "Unknown", value: `ID: \`${content.id || "<redacted>"}\`\n\nWorth ${content.price || 0} <:cp:744403130594230313>. \n${content.desc || "<redacted>"}\nThis item have the rarity of **${rarity || "unknown"}**.`, inline: true})
                         
                     }
+                        }
                     } else {
-                        let fields = [];
+                        
                         imports._.each(item, (value, key) => {
                             if(key === "assets") {
                                 value = "Add \"--content\" to the back, yeah.";
@@ -53,6 +53,10 @@ module.exports = {
                 
             
         } else {
+            const bundles = imports.getItem("bundle", null, "all").filter((bundle) => {
+            if (bundle.available && bundle.available === false) return false;
+            return true;
+        })
             for (let bundle of bundles) {
                 let user_can_buy;
                 if (user.cp && user.cp !== 0 && bundle.price && (user.cp - bundle.price) >= 0) {
@@ -61,10 +65,10 @@ module.exports = {
                     user_can_buy = "**Sadly, you doesn't have enough funds. :cry:**\n";
                 }
 
-                embed = embed.addField(bundle.name || "Unknown", imports.trim(`ID: \`${bundle.id || "<redacted>"}\`\n\n${bundle.desc || "<redacted>."} \nThis bundle costs ${bundle.price || "<redacted>"} <:cp:744403130594230313>. ${user_can_buy}`, 1024), true);
+                fields.push({name: bundle.name || "Unknown", value: imports.trim(`ID: \`${bundle.id || "<redacted>"}\`\n\n${bundle.desc || "<redacted>."} \nThis bundle costs ${bundle.price || "<redacted>"} <:cp:744403130594230313>. ${user_can_buy}`, 1024), inline: true});
             }
         }
-
+   embed = embed.addFields(fields);
         return imports.message.channel.send(embed);
     }
 
