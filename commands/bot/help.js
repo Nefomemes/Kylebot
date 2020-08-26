2a28cbae2d60c173a174d241c3a2b939c3075d9a
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-08-26 15:03:59 +0700

    Update help.js

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -12,7 +12,7 @@ module.exports = {
             .setThumbnail(imports.client.user.displayAvatarURL({ format: "png", dynamic: true }))
             .setTimestamp()
             .setFooter(`Prefix: ${imports.prefix} | ${imports.getRandomFunfact()}`, imports.client.user.displayAvatarURL({ format: "png", dynamic: true }))
- 
+         var fields = [];
       var commands = imports.client.commands.cache.map(i => i);
             function getCategory(name) {
             if (!name) return;
@@ -32,13 +32,10 @@ module.exports = {
             }
         }
         if(imports.getCommand(form, imports.client)){
+         let command = imports.getCommand(form, imports.client);
             if(imports.args[0] && imports.args[0].toLowerCase() === "-args" && imports.getCommand(form, imports.client).args && imports.getCommand(form, imports.client).args.constructor === Array){
-
-                let number = parseInt(imports.args[0] || 1);
-                if (Number.isNaN(number) || !number){
-                    number = 1;
-                }
-                let command = imports.getCommand(form, imports.client);
+             imports.args.shift();
+                
                 for(let arg of command.args){
                     let optional;
                     if(arg.optional && arg.optional === true){
@@ -49,53 +46,36 @@ module.exports = {
                      optional = arg.optional || "This argument is ¯\\_(ツ)_/¯.";
                     }
 
-                    embed = embed.addField(arg.name || "Unknown", imports.trim((arg.desc || "<redacted> ") + "\n\n" + optional, 1024), true);
+                    fields.push({name: arg.name || "Unknown", value: imports.trim((arg.desc || "<redacted> ") + "\n\n" + optional, 1024), inline: true});
                 
                 }
             } else {
-       /*  let number = parseInt(imports.args[0] || 1);
-                if (Number.isNaN(number) || !number){
-                    number = 1;
-                }*/
-                let command = imports.getCommand(form, imports.client);
-              
+                 
                 imports._.each(command, function(value, key){
                     if(key.toLowerCase().startsWith("desc"))return embed = embed.setDescription(value.toString());
-                    if(key.toLowerCase() === "args" )return embed = embed.addField("Arguments", imports.trim("Please add \"-args\" after the syntax you are using. Example, `<prefix>help user -args`.", 1024), true);
+                    if(key.toLowerCase() === "args" ) value = "Use \"-args\" to view arguments, yeah.";
                     if(key === 'run')return;
-                    embed = embed.addField(key.toString(), value.toString(), true);
+                    fields.push({name: key, value: value, inline: true});
                 })
             }
   
     
         } else if(getCategory(form)){
-            // Category 
-            let number = parseInt(imports.args[0] || 1);
-            if (Number.isNaN(number) || !number){
-                number = 1;
-            }
-            commands = commands.filter( filterTheCommands(getCategory(form).id));
-            let page = imports.getPage(commands, 25, number);
-            
+          
+               const commands = filterTheCommands(getCategory(form).id));
+      
             commands.forEach((command) => {
-                let index = commands.indexOf(command);
-                if( index > page.end || index < page.start) return;
+             
         let name = imports.client.commands.cache.findKey(i => i === command);
-        embed = embed.addField(name || "Unknown", command.desc || "No description", true);
+        embed = embed.addField({name: name || "Unknown", value: command.desc || "No description", inline: true});
             });
         } else {
         embed = embed.setImage(imports.brandingbg);
        embed = embed.setDescription("Kylebot is currently the first Call of Duty roleplay Discord bot.")
-        
-            // All categories
-            let number = parseInt(form);
-            if (Number.isNaN(number) || !number){
-                number = 1;
-            }
-            let page = imports.getPage(categories, 25, number);
+ 
             categories.forEach((category) => {
-                let index = categories.indexOf(category);
-                if( index > page.end || index < page.start) return;
+               
+      
                 embed = embed.addField(category.name, imports.trim(`ID: \`${category.id}\` \n\n${category.desc || "No description."} \n\n${commands.filter(filterTheCommands(category.id)).length} commands available.`, 2048), true);
             })
         
