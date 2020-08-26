be45ab18a8f2060480142986680532e5c27e17b2
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-08-26 20:21:12 +0700

    Update channel.js

--- commands/server-thingies/channel.js
+++ commands/server-thingies/channel.js
@@ -1,12 +1,12 @@
-module.exports = {
-    name: "channel",
-    execute(imports) {
-        return;
-        var channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first() || message.channel;
-
-        if (channel) return message.reply("that isn't a valid channel!");
-
-
+module.exports.run = async (imports) => {
+       
+        var channel = imports.getChannelFromMention(imports.args[0]);
+if(channel) imports.args.shift();
+    if(!channel) channel = imports.message.channel;
+    
+    var embed = new imports.Discord.MessageEmbed()
+    .setColor(imports.colors.BG_COLOR)
+   .setTitle()
 
 
     }
