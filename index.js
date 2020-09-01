b4fa7bb23f45dcaa36b4a136248bd12c77ca7273
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-09-01 17:40:58 +0700

    GuildMemberManager#fetchMemberFromMember() DJS addon

--- index.js
+++ index.js
@@ -34,6 +34,29 @@ global.querystring = require("querystring");
 global.fetch = require("node-fetch");
 global.grau = require("node-grau");
 const db = new global.grau(process.env.DB, 'bot');
+
+global.Discord.Guild.prototype.members.fetchMemberFromMention = (m) => {
+  if(!m || m.constructor !== String) return;
+  var id = m;
+  if(id.startsWith("<@") && id.endsWith(">") id.slice(2, id.length - 1);
+  if(id.startsWith("!")) id.slice(1);
+  if(!Number.isNaN(parseInt(id))){
+  return this.fetch(id)
+    } else {
+  return this.cache.find((member) => {
+    if(member.displayName.split(m)[1]) return true;
+    if(member.user.username.split(m)[1]) return true;
+    return false;
+    })     
+  
+}
+}
+global.Discord.Client.prototype.users.fetchUserFromMention = (m) => {
+  
+ 
+  }
+
+
 function CommandsManager(cache) {
   this.cache = cache;
 }
