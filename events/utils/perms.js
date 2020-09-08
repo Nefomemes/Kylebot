f432dd652fbbfc5d877515ee8a0c963ad54d56f8
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-09-08 16:29:43 +0700

    Update perms.js

--- events/utils/perms.js
+++ events/utils/perms.js
@@ -18,24 +18,34 @@ switch(permsLevel){
         return true;
     case 1:
         if(member.permissions.has("MANAGE_EMOJIS"))return true;
+        break;
     case 2:
         if(member.permissions.has("MANAGE_NICKNAMES"))return true;
+        break;
     case 3:
         if(member.permissions.has("KICK_MEMBERS"))return true;
+        break;
     case 4:
         if(member.permissions.has("BAN_MEMBERS"))return true;
+        break;
     case 5:
         if(member.permissions.has("MANAGE_CHANNELS"))return true;
+        break;
     case 6:
         if(member.permissions.has("MANAGE_ROLES"))return true;
+        break;
     case 7:
         if(member.permissions.has("MANAGE_GUILD"))return true;
+        break;
     case 8:
         if(member.permissions.has("ADMINISTRATOR")) return true;
+        break;
     case 9:
         if(member.user.id === member.guild.ownerID)return true;
+        break;
     case 10:
     if(member.client.admins.cache.get(member.user.id))return true;
+        break;
    
   
   default:
