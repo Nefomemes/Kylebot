86fd328bc14dd3663b9fa063043469b850e8fea9
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-09-21 10:43:05 +0700

    Update configs.js

--- assets/configs/configs.js
+++ assets/configs/configs.js
@@ -1,8 +1,9 @@
-const web = "https://web.nefomemes.repl.co/kylebot";
+const web = "https://api.nefomemes.repl.co/links/kylebot";
 module.exports = {
-    "prefix": "wz!",
+    "prefix": "mw!",
     "website": web,
     "support": `${web}/support`,
+    "invite":`${web}/invite?perms=8`,
     "brandingbg": "https://i.imgur.com/q3EWSPl.gif",
     "owners":["665419057075585025"]
 }
