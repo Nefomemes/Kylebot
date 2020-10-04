4f09a2dd2e83455ce3257d68704e4f5c29b7009e
Nefomemes <nefothingy@hotmail.com>
2020-10-04 13:21:14 +0000

    k

--- supcommands/mw/weapon.js
+++ supcommands/mw/weapon.js
@@ -49,7 +49,7 @@ module.exports = {
                     weapon = i.argv.weapon
                 } else {
                     let foobar = [];
-                    for (const [key, value] of Object.entries(weaponStats)) {
+                    for (const [key, value] of Object.entries(weapons)) {
                         foobar.push({ key: key, value: value });
                     }
                     foobar = foobar.filter((oj) => {
