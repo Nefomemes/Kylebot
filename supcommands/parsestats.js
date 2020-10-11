a3cd813dff0731624b33e301e750748b6ea49d97
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-10-11 07:19:10 +0700

    Update parsestats.js

--- supcommands/parsestats.js
+++ supcommands/parsestats.js
@@ -1,5 +1,3 @@
-
-
 module.exports = (value, key, fields) => {
     switch (key.toLowerCase()) {
         case "kills":
@@ -8,20 +6,22 @@ module.exports = (value, key, fields) => {
             return fields.push({ name: 'Deaths', value: `${value} deaths`, inline: true });
         case "score":
             return fields.push({name: "Score", value: `${value}`, inline: true});
-        case "timePlayed":
+        case "timeplayed":
             return fields.push({name: "Time played", value: `${value}`, inline: true});
-        case "scorePerMinute":
+        case "scoreperminute":
             return fields.push({name: "SPM", value: `${value} scores/minute`, inline: true});
-        case "kdRatio":
+        case "kdratio":
             return fields.push({name: "KD Ratio", value: `${value}`, inline: true});
         case "headshots":
             return fields.push({name: "Headshots", value: `${value} times`, inline: true});
-        case "distanceTraveled":
+        case "distancetraveled":
             return fields.push({name: "Distance traveled", value: `${value}`, inline: true});
-        case "damageDone":
+        case "damagedone":
             return fields.push({name: "Damage done", value: `${value}`, inline: true});
-        case "killsPerGame":
+        case "killspergame":
             return fields.push({name: "Kills per game", value: `${value} kills`, inline: true});
+        case "wallbangs":
+            return fields.push({name: "Wallbangs", value: `${value} times`, inline: true});
             default:
            return fields.push({name: key, value: value, inline: true});
     }
