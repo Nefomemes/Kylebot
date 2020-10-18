cedc38114e2b0bf98fe0c81a3fe8e131778552ea
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-10-18 16:59:11 +0700

    ok

--- supcommands/configs/welcomemessage.js
+++ supcommands/configs/welcomemessage.js
@@ -3,6 +3,7 @@ module.exports = {
 	description: "Configure the message that will be sent when a member joined.",
 	docs: "https://github.com/Nefomemes/docs/blob/main/Kylebot/super-commands/configs.md#prefixconfigs-welcomemessage-welcomemessage",
 	run: async i => {
+		
  if(!i.args.length) return i.message.channel.send("Unlike some other commands, you just need to type the message content that will be sent whenever a user joined the server.");
  
  if(i.args.join(" ").length > 500) return i.message.channel.send("To prevent abuse and to save database resources, we restrict message content to only 500 characters.");
