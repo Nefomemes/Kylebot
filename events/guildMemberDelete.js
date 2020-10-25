261f3bf657b56cb9443dc7d0344b0d14e18f9075
ForTombstone <nefothingy@hotmail.com>
2020-10-25 04:17:32 +0000

    ok

--- events/guildMemberDelete.js
+++ events/guildMemberDelete.js
@@ -1,10 +1,10 @@
-const { Db } = require("mongodb");
+
 
 module.exports = async (member) => {
 	try {
 		const guildDB = await db.collection("guilds").getDoc({ docID: member.guild.id });
 		if (guildDB.goodbyeChannel && guildDB.goodbyeMessage) {
-			const channel = await client.channels.fetch(guildDB.goodbyeChannel);
+			const channel = await client.channels.cache.get(guildDB.goodbyeChannel);
 			if (channel) {
 				channel.send(built_ins.trim(`*The content of this message is managed by an end user. We do not take responsibility of any content in this message.*\n\n${guildDB.goodbyeMessage.split("${member}").join(`${member}`).split("${count}").join(`${member.guild.memberCount}`).split("${username}").join(`${member.user.username}`).split("${discriminator}").join(`${member.user.discriminator}`).split("${displayName}").join(`${member.displayName}`)}`, 2000))
 		}
