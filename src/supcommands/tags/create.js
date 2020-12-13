d02307e0bfb1fcc16d745b5187309789d8a6f370
ForTombstone <nefothingy@hotmail.com>
2020-12-13 09:55:26 +0000

    ok

--- src/supcommands/tags/create.js
+++ src/supcommands/tags/create.js
@@ -11,12 +11,13 @@ module.exports = {
 	if(!i.argv.t) return i.message.channel.send(
 		"-t > Tag content."
 	);
-
-	var scanNAme = (io, n = i.argv.n) => io.name.toLowerCase() === n.toLowerCase() || i.aliases && i.aliases.find((oi) => oi.toLowerCase() === n.toLowerCase()) || i.id === n;
+	
+	i.argv.n = i.argv.n.toLowerCase();
+	
 	var tags = quickdb.get("bot.tags");
 	if(!tags) tags = quickdb.set("bot.tags", []);
  
-
+var scanName = (io, n = i.argv.n) => io.name.toLowerCase() === n.toLowerCase() || i.aliases && i.aliases.find((oi) => oi.toLowerCase() === n.toLowerCase()) || i.id === n;
 	if(tags.find(scanName)) return i.message.channel.send("The name you trying to use is currently unavailable. Try another name.");
 
 	if(i.argv.n.startsWith("TAG") && !Number.isNaN(parseInt(alias.slice(2)))) return i.message.channel.send("It seems the name you are trying use matched up with the reserved `TAG{number}` syntax that is used for tag IDs. Try another name.");
